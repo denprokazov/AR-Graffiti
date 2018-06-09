79344d303d3bf2865ed2e269370c38fb59ef1be1
somborero <denprokazov@gmail.com>
2018-06-09 16:50:10 +0300

    Added main activity with mapbox.

--- app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/MainActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/MainActivity.java
@@ -1,10 +1,17 @@
 package com.google.ar.sceneform.samples.hellosceneform;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.PolygonOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.map_screen);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(mapboxMap -> drawPolygon(mapboxMap, new LatLng(45.522585,-122.685699)));
+    }
+
+    private void drawPolygon(MapboxMap mapboxMap, LatLng center) {
+        List<LatLng> polygon = new ArrayList<>();
+        double delta = 0.005;
+        polygon.add(new LatLng(center.getLatitude()+delta, center.getLongitude()-delta));
+        polygon.add(new LatLng(center.getLatitude()+delta, center.getLongitude()+2*delta));
+        polygon.add(new LatLng(center.getLatitude()-2*delta, center.getLongitude() + 2*delta));
+        polygon.add(new LatLng(center.getLatitude()-2*delta, center.getLongitude()-2*delta));
+        polygon.add(new LatLng(center.getLatitude()+delta, center.getLongitude()-2*delta));
+        mapboxMap.addPolygon(new PolygonOptions()
+                .addAll(polygon)
+                .fillColor(Color.parseColor("#3bb2d0")));
     }
 
     @Override
